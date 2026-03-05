ab9063d618d0ea9abf3ef93bcef022423adeaace
Luidy Costa <lluidycosta2024@gmail.com>
2026-03-05 12:10:28 -0300

    style: tela painel inicial pos login medico
    O painel inicial pos login do perfil medico ainda sem interação com a pi sendo apenas ilustrativo

--- lung-cancer-ml/frontend/src/App.jsx
+++ lung-cancer-ml/frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginSelecao from './pages/LoginSelecao';
 import LoginMedico from './pages/LoginMedico';
 import CadastroMedico from './pages/CadastroMedico';
-import RecuperarSenha from './pages/RecuperarSenha'; // <--- Importe aqui
+import RecuperarSenha from './pages/RecuperarSenha';
+import PainelMedico from './pages/PainelMedico'; // <--- Importe a nova tela
 
 function App() {
   return (
@@ -11,7 +12,10 @@ function App() {
         <Route path="/" element={<LoginSelecao />} />
         <Route path="/login-medico" element={<LoginMedico />} />
         <Route path="/cadastro-medico" element={<CadastroMedico />} />
-        <Route path="/recuperar-senha" element={<RecuperarSenha />} /> {/* <--- Atualize aqui */}
+        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+        
+        {/* Rota da área logada */}
+        <Route path="/painel-medico" element={<PainelMedico />} /> 
       </Routes>
     </BrowserRouter>
   );
